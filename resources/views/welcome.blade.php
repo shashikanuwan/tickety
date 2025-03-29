b403fa121475e5b7c315b6c4b4c5aa7e3d976043
shashikanuwan <kumararanaweera1999@gmail.com>
2025-03-30 03:21:23 +0530

    feat: add dark/light mode toggle and login button to home page
    - Implemented dark/light mode toggle for better user experience
    - Styled and positioned the login button on the home page
    - Ensured smooth transition between themes using Tailwind CSS
    - Improved accessibility and UI responsiveness

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,15 @@
     <div class="container mx-auto px-4 py-8">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <div>
-                <x-ticket.support-form />
+                <x-ticket.support-form/>
             </div>
             <div>
-                <livewire:ticket.search-ticket-form :referenceNumber="request('ref')" />
+                <livewire:ticket.search-ticket-form :referenceNumber="request('ref')"/>
             </div>
         </div>
     </div>
 
+    <x-login-button/>
+    <x-mode-button/>
     <x-ticket.footer/>
 </x-guest-layout>
